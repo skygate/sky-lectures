bb9db0fa18671c21392361e6a95d138a94afbffe
karol@szuster.info <karol@szuster.info>
2021-08-25 10:09:25 +0200

    [SK-3] create custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["first_name", "last_name", "email"]
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def has_module_perms(self, users):
+        return self.is_superuser
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
