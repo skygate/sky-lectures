8d985e4b33db6552ed88f3d0c0ff044c31492493
Marcin Mistarz <marcin.mistarz@skygate.io>
2021-08-25 14:38:08 +0200

    [SK-20] Optimize imports, load different urls for dev and prod

--- backend/sky_lectures/urls.py
+++ backend/sky_lectures/urls.py
@@ -13,30 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions, routers, serializers, viewsets
-from drf_yasg.views import get_schema_view
+from django.urls import path
 from drf_yasg import openapi
-from django.contrib.auth.models import User
+from drf_yasg.views import get_schema_view
+import sky_lectures.settings.base
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="sky-lectures",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="Application keeping Sky Gate presentations",
    ),
    public=True,
-   permission_classes=[permissions.AllowAny],
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+if (sky_lectures.settings.base.DEBUG == True):
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+    ]
+else:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ]
