7e6eb29f323892ef7454c08c893cf484185942c8
michal-kolodziejczyk <95228210+michal-kolodziejczyk@users.noreply.github.com>
2021-12-02 13:11:34 +0100

    feat: SLF-9 - create state management (#22)
    * feat: SLF-9 - create state management
    
    * feat:SLF-9 create Interface and add Provider into index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/SignIn" element={<div>SignIn</div>} />
-        <Route path="/SignUp" element={<div>SignUp</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/SignIn" element={<div>SignIn</div>} />
+          <Route path="/SignUp" element={<div>SignUp</div>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
